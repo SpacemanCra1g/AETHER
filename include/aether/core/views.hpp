751d0306906e17ce3cbc1e9cccbdaf6ac18c5c16
nate <nvanduker@gmail.com>
2025-10-12 19:11:55 -0700

    Flux and Cell view, data, and container objects added in /include/aether/core/views.hpp

--- include/aether/core/views.hpp
+++ include/aether/core/views.hpp
@@ -1,5 +1,6 @@
 #pragma once 
 #include <array>
+#include <iterator>
 #include <vector>
 #include <cstddef>
 #include <cassert>
@@ -9,18 +10,213 @@
 
 namespace aether::core {
 
-// ---------- Start with a generic Cell Centered array structure ----------
+// ---------- Start with a generic Cell View structure ----------
 // ---------- Template on the number of components (compile time known, physics & dim) ----------
 template<int NCOMP> 
 struct CellsViewT{
     std::array<double*, NCOMP> comp{};  // comp[NCOMP][Flat_index]
     Extents ext;                        // Extents struct defined in strides.hpp
 
-    AETHER_INLINE std::size_t idx(int i, int j, int k) const {return ext.index(i,j,k);} // Contains the boundary checking built into Extents 
-    
+    AETHER_INLINE std::size_t idx(int i, int j=0, int k=0) const {return ext.index(i,j,k);} // Contains the boundary checking built into Extents 
+
+    // Access with fast flat accessor 
+    AETHER_INLINE double & var(int c, std::size_t flat) {return comp[c][flat];}
+    // ---------- Access overload for const instantiations of the Cell View ----------    
+    AETHER_INLINE const double & var(int c, std::size_t flat) const {return comp[c][flat];}
+
+    // Access with index function
+    AETHER_INLINE double & var(int c, int i, int j = 0, int k = 0) {return comp[c][idx(i,j,k)];}
+    // ---------- Access overload for const instantiations of the Cell View ----------    
+    AETHER_INLINE const double & var(int c, int i, int j = 0, int k = 0) const {return comp[c][idx(i,j,k)];}
+};
+
+// ---------- Continue with a generic Cell container structure ----------
+template<int NCOMP>
+struct CellsSoAT {
+    std::array<std::vector<double>,NCOMP> comp; 
+    Extents ext; 
+
+
+    CellsSoAT() = default; // Default empty constructor 
+
+    // ---------- Constructor creates Extents struct and ----------
+    // ---------- Allocates the flat array comp[NumberOfVariables][Total_Cells_in_T_Domain] ----------
+    CellsSoAT(int nx, int ny, int nz, int ng) : ext(nx,ny,nz,ng){
+        const std::size_t N = size_t(ext.Nx) * ext.Ny * ext.Nz; 
+        for (int c = 0; c < NCOMP; ++c) comp[c].resize(N);
+    }
+
+    // ---------- Returns the total flattened size of each variable array ----------
+    AETHER_INLINE std::size_t size_flat() const{
+        return std::size_t(ext.Nx) * ext.Ny * ext.Nz;
+    }
+
+    // ---------- Returns the CellsView template, used for cell access ----------
+    AETHER_INLINE struct CellsViewT<NCOMP> view() {
+        struct CellsViewT<NCOMP> v; 
+        v.ext = ext; 
+        for (int c = 0; c < NCOMP; ++c) v.comp[c] = comp[c].data();
+        return v;
+    }
+};
+
+// -------------------------------------------------------------
+// ----- Now we make similar containers for the flux faces -----
+// ------------- Containers have the flat structure  -----------
+// -------------------------------------------------------------
+// ---------- For the X-Faces        (Nx+1)*Ny*Nz --------------
+// ---------- For the Y-Faces        Nx*(Ny+1)*Nz --------------
+// ---------- For the Z-Faces        Nx*Ny*(Nz+1) --------------
+// -------------------------------------------------------------
+
+struct FaceGridX{
+int NxF{0}, Ny{0}, Nz{0};
+std::size_t sy{0}, sz{0};
+
+FaceGridX() = default; 
+explicit FaceGridX(const Extents &e){
+    NxF = e.Nx + 1; Ny = e.Ny; Nz = e.Nz;
+    sy = std::size_t(NxF); sz = std::size_t(NxF)*Ny;
+}
+
+AETHER_INLINE std::size_t index(int iF, int j, int k) const {
+#if AETHER_BOUNDS_CHECK
+    assert(iF >= 0 && iF < NxF);
+    assert(j  >= 0 && j  < Ny);
+    assert(k  >= 0 && k  < Nz);
+#endif 
+
+    return std::size_t(iF) + std::size_t(j)*sy + std::size_t(k)*sz;
+}
+
+AETHER_INLINE std::size_t nfaces() const{ return std::size_t(NxF)*Ny*Nz;}
+};
+
+struct FaceGridY{
+int Nx{0}, NyF{0}, Nz{0};
+std::size_t sy{0}, sz{0};
+
+FaceGridY() = default; 
+explicit FaceGridY(const Extents &e){
+    Nx = e.Nx; NyF = e.Ny+1; Nz = e.Nz;
+    sy = std::size_t(Nx); sz = std::size_t(Nx)*NyF;
+}
+
+AETHER_INLINE std::size_t index(int i, int jF, int k) const {
+#if AETHER_BOUNDS_CHECK
+    assert(i >= 0 && i < Nx);
+    assert(jF  >= 0 && jF  < NyF);
+    assert(k  >= 0 && k  < Nz);
+#endif 
+
+    return std::size_t(i) + std::size_t(jF)*sy + std::size_t(k)*sz;
+}
+
+AETHER_INLINE std::size_t nfaces() const{ return std::size_t(Nx)*NyF*Nz;}
+};
+
+struct FaceGridZ{
+int Nx{0}, Ny{0}, NzF{0};
+std::size_t sy{0}, sz{0};
+
+FaceGridZ() = default; 
+explicit FaceGridZ(const Extents &e){
+    Nx = e.Nx; Ny = e.Ny+1; NzF = e.Nz;
+    sy = std::size_t(Nx); sz = std::size_t(Nx)*Ny;
+}
+
+AETHER_INLINE std::size_t index(int i, int j, int kF) const {
+#if AETHER_BOUNDS_CHECK
+    assert(i >= 0 && i < Nx);
+    assert(j  >= 0 && j  < Ny);
+    assert(kF  >= 0 && kF  < NzF);
+#endif 
+
+    return std::size_t(i) + std::size_t(j)*sy + std::size_t(kF)*sz;
+}
+
+AETHER_INLINE std::size_t nfaces() const{ return std::size_t(Nx)*Ny*NzF;}
+};
+
+
+// -------------------------------------------------------------
+// ----- SoA Data containers for the flux faces + Quadrature ---
+// ------------- Containers have the flat structure  -----------
+// -------------------------------------------------------------
+// ------------------ [comp][face * Q + q] ---------------------
+// -------------------------------------------------------------
+
+
+// ---------- First, define light quadrature struct  -----------
+// ---------- Contains number of quadrature points per face  -----------
+
+struct Quadrature {
+int Q{1}; // Default to 1 quadrature point.
+};
+
+// ---------- Template for Flux Face views ----------
+template<int NCOMP>
+struct FaceArrayViewT {
+    std::array<double *, NCOMP> comp;
+    std::size_t nfaces{0};
+    int Q{1};
+
+    AETHER_INLINE std::size_t flat(int face_idx, int q) const {
+
+#if AETHER_BOUNDS_CHECK        
+    assert(std::size_t(face_idx) < nfaces && face_idx >= 0);
+    assert(q >= 0 && q < Q);
+#endif
+
+    return std::size_t(face_idx)*Q + q;
+    }
+
+    AETHER_INLINE double& var(int c, int face_idx, int q) {return comp[c][flat(face_idx,q)];}
+
+    // ---------- Access overload for const instantiations of the Flux View ----------
+    AETHER_INLINE const double& var(int c, int face_idx, int q) const {return comp[c][flat(face_idx,q)];}
+};
+
+
+// ---------- Owner and Container of Flux's ----------
+template <int NCOMP>
+struct FaceArraySoAT{
+    std::array<std::vector<double*>, NCOMP> comp;
+    std::size_t nfaces{0};
+    int Q{1};
+
+    // ---------- Constructors ----------
+    FaceArraySoAT() = default; 
+
+    // ---------- Overloads for each of the flux face types ----------
+    explicit FaceArraySoAT(const FaceGridX& g, Quadrature quad) : nfaces(g.nfaces()), Q(quad.Q){
+        const std::size_t N = nfaces*std::size_t(Q);
+        for (int c = 0; c < NCOMP; ++c) comp[c].resize(N);
+    }
+
+    explicit FaceArraySoAT(const FaceGridY& g, Quadrature quad) : nfaces(g.nfaces()), Q(quad.Q){
+        const std::size_t N = nfaces*std::size_t(Q);
+        for (int c = 0; c < NCOMP; ++c) comp[c].resize(N);
+    }
+
+    explicit FaceArraySoAT(const FaceGridZ& g, Quadrature quad) : nfaces(g.nfaces()), Q(quad.Q){
+        const std::size_t N = nfaces*std::size_t(Q);
+        for (int c = 0; c < NCOMP; ++c) comp[c].resize(N);
+    }
+
+    AETHER_INLINE FaceArrayViewT<NCOMP> view(){
+        struct FaceArrayViewT<NCOMP> v; 
+        v.Q = Q; v.nfaces = nfaces;
+        for (int c = 0; c < NCOMP; ++c) v.comp[c] = comp[c].data();
+        return v;
+    }
 
 };
 
+// ---------- Quick and dirty overload to get face index, for any grid type ----------
+AETHER_INLINE int face_index(FaceGridX& gx, int iF, int j, int k) {return int(gx.index(iF, j, k));}
+AETHER_INLINE int face_index(FaceGridY& gy, int i, int jF, int k) {return int(gy.index(i, jF, k));}
+AETHER_INLINE int face_index(FaceGridZ& gz, int i, int j, int kF) {return int(gz.index(i, j, kF));}
 
 }
 
